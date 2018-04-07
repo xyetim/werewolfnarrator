2702204421790fd1a307d0aedb92e94a4e41c925
Knarf7474 <fpvanbeem@gmail.com>
2018-04-07 10:34:27 +0200

    start of app

---
+++ db/migrate/20180405065511_create_players.rb
@@ -0,0 +1,10 @@
+class CreatePlayers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.references :game
+
+      t.timestamps
+    end
+  end
+end
